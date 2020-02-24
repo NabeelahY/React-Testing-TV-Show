540d62230e9383e6025764d759e0d86209209733
Nabeelah <noblenab13@gmail.com>
2020-02-24 18:33:09 +0100

    Got episode test working

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -3,30 +3,46 @@ import Episodes from './Episodes';
 import * as rtl from '@testing-library/react';
 
 // jest.mock('axios', () => {
-//   // the callback retuns something
-//   // we return the "new", artificial axios module
-//   // { get: Function returns promise, post: Funcion... }
 //   return {
-//     get: (url) => {
-//       return Promise.resolve({ data: [] })
-//     }
-//   }
-// })
+//     get: url => {}
+//   };
+// });
+const ep = [
+  {
+    id: 553946,
+    url:
+      'http://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers',
+    name: 'Chapter One: The Vanishing of Will Byers',
+    season: 1,
+    number: 1,
+    airdate: '2016-07-15',
+    airtime: '',
+    airstamp: '2016-07-15T12:00:00+00:00',
+    runtime: 60,
+    image: {
+      medium:
+        'http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg',
+      original:
+        'http://static.tvmaze.com/uploads/images/original_untouched/67/168918.jpg'
+    },
+    summary:
+      "<p>A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.</p>",
+    _links: { self: { href: 'http://api.tvmaze.com/episodes/553946' } }
+  }
+];
 
 describe('Episode', () => {
   let wrapper;
   afterEach(rtl.cleanup);
 
   beforeEach(() => {
-    const episode = Array.prototype;
-    wrapper = rtl.render(<Episodes episodes={episode} />);
-    console.log(wrapper)
+    wrapper = rtl.render(<Episodes episodes={ep} />);
   });
 
   it('it displays without crashing', async () => {
-    const episodesDiv = await wrapper.findByText('Chapter Two: The Mall Rats');
-    console.log(episodesDiv)
-    // expect(episodesDiv).toBeInTheDocument();
+    const episodesDiv = await wrapper.findByText(
+      'Chapter One: The Vanishing of Will Byers'
+    );
     expect(episodesDiv).toBeVisible();
   });
 });
